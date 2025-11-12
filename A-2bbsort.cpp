beef7fed9eda78d9d3e38eacd45374858240741e
devcpp1983 <aherpratik630@gmail.com>
2025-11-12 15:11:40 +0530

    Update A-2bbsort.cpp

--- A-2bbsort.cpp
+++ A-2bbsort.cpp
@@ -2,40 +2,38 @@
 #include <algorithm>
 using namespace std;
 
-void selectionsort( float arr[], int n) {
+void bubblesort( float arr[], int n) {
 	for( int i = 0; i < n-1; i++) {
-		int minindex = i;
-		for( int j = i+1; j < n; j++) {
-			if( arr[j] < arr[minindex])
-				minindex = j;
+		for( int j = 0; j < n-i-1; j++) {
+			if ( arr[j] > arr[j+1]) 
+				swap( arr[j], arr[j+1]);
 		}
-		swap( arr[i], arr[minindex]);
 	}
 }
 
 void display ( float arr[], int n) {
 	cout << "TOP FIVE \n";
-	for( int i = n-1; i >= max(n-5,0); i--) {
-	cout << arr[i] << endl;
-	}
+	for(int i = n-1; i >= max(n-5,0); i--) {
+		cout << arr[i] << endl;
+	} 
 }
 
 int main() {
 	int n;
-	
-	cout << "enter no of students :\n";
+	cout << "enter the number of students :";
 	cin >> n;
 	
 	float arr[n];
 	
 	cout << "enter percentages :\n";
-	for( int i = 0; i < n; i++) {
+	for( int i=0; i <n; i++) {
 		cin >> arr[i];
-	}
+	}	
 	
-	selectionsort( arr, n);
-	display( arr, n);
+	bubblesort( arr, n);
+	display( arr,n);
+	
+	
 	
 	return 0;
-	
 }
